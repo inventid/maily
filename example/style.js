a7ec212ac6205a8b87c0db65c2990ece2b28201a
Rogier Slag <rogier.slag@gmail.com>
2016-07-11 15:17:07 +0200

    Improved example

--- example/style.js
+++ example/style.js
@@ -3,15 +3,24 @@ module.exports = {
     fontSize: '20px'
   },
 
+  distance: '12', // px
+
   footer: {
     socialIcon: {
       size: '16px'
     }
   },
 
+  width: {
+    half: '50%',
+    full: '100%'
+  },
+
   colors: {
+    white: '#FFFFFF',
     primary: '#2e7d32',
     secondary: '#42a5f5',
-    tertiary: '#f5f5f5'
+    tertiary: '#f5f5f5',
+    black: '#080808'
   }
 };
